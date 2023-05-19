1a4befc67bb8d52d4e4b1e22cf43093ad9dcc54e
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-05-18 23:18:47 -0500

    finished off javascript file

--- index.js
+++ index.js
@@ -1,43 +1,90 @@
-var generateB = document.querySelector('#generate');
-var numArray = [1, 2 ,3 ,4 ,5 ,6 , 7, 8, 9, 0];
-var lowLetterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
+var generateBtn = document.querySelector('#generate');
+
+var charLower = ['a', 'b', 'd', 'c' ,'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var spec = ['!', '@', '#', '%', '^', '&', '*'];
+var charUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+function createPasswordOptions(){
+    var passwordLength = prompt(
+        'How long would you like your password to be? (Required to be between 10 - 128 characters');
+
+
+        if  (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 128) {
+
+            alert('please try again, password length must be 10 - 128 characters in length.'
+            );
+            writePassword()
+            return 
+        }
+    
+    var includeSpeci = confirm('Include Special Characters?');
+
+    var includeCharUp = confirm('Include Uppercase Letters?')
+
+    var includeCharLc = confirm('Include Lowercase Letters?');
+
+    var includeNum = confirm('Include Numbers?');
+
+    var userOptions = {
+        length: passwordLength,
+        hasUpper: includeCharUp,
+        hasLower: includeCharLc,
+        hasNum: includeNum,
+        hasSpecial: includeSpeci
+    }
+
+       
+
+     return userOptions   
+
+
+    }
+function generatePassword(){
+    var options = createPasswordOptions();
+    
+    var selectedCharacters = []
+    var genPassword = "";
+    var temp;
+    if (options.hasLower) {
+        selectedCharacters = selectedCharacters.concat(charLower)
+    }
+    if (options.hasUpper) {
+        selectedCharacters = selectedCharacters.concat(charUpper)
+    }
+    if (options.hasSpecial) {
+        selectedCharacters = selectedCharacters.concat(spec)
+    }
+    if (options.hasNum) {
+        selectedCharacters = selectedCharacters.concat(num)
+    }
+    console.log(selectedCharacters)
+    console.log(options.length)
+    for (index = 0; index < options.length; index++) {
+        temp = Math.floor(Math.random() * selectedCharacters.length);
+        genPassword = genPassword + selectedCharacters[temp];
+    }
+    
+    return genPassword
+    
+}
 
 
 function writePassword(){
-    var passsword = generatePassword();
-    var passwordText = document.querySelector('#passsword');
-
-    passwordText.value = 'password';
-}
-
-function generatePassword(){
-    var password = [];
+    var password = generatePassword();
     var passwordText = document.querySelector('#password');
-    var passwordOptions = createPasswordOptions();
+    passwordText.value = password;
+    
 }
-function createPasswordOptions(){
-    var passwordLength = createPasswordLength();
+    
 
-    var specialCharacters = confirm('Click OK to confirm including special characters.');
-}
+ 
 
-function createPasswordLength(){
-    var passwordLength = prompt(
-    'how long would you like your password to be? (Required to be between 10 - 128 characters');
 
-    createPasswordLength();
 
-    if  (passwordLength < 10 || passwordLength > 128) {
-        alert(
-            'please try again, password length must be 10 - 128 characters in length.'
-        );
-        createPasswordLength();
-    }
 
-return passwordLength;
-}
 
-var generateBtn = document.querySelector('#generate');
 
-generateBtn.addEventListener('click', writePassword);
+
+generateBtn.addEventListener('click', writePassword)
